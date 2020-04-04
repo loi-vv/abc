562359d1d41413cb2aaadbfbd34e67ec71b628dc
loi.vv <vuvanloi150496@gmail.com>
2020-04-04 17:32:37 +0700

    Use hbs engine

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var hbs = require('express-handlebars');  
 require('dotenv').config();
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 
 app.use(logger('dev'));
 app.use(express.json());
